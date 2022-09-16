7408af24662fb5ff64f113c352d746e602b2f21a
David Sobral <davidsobral@me.com>
2022-09-16 15:28:43 -0300

    Div contents animation plays

--- js/landing.js
+++ js/landing.js
@@ -6,37 +6,44 @@ let played = false
 // Get elements to animate
 const logoAnimation = document.getElementById('top-logo').getAnimations()[0]
 const menuAnimation = document.getElementById('menu').getAnimations()[0]
-const divAnimation = document.getElementById('wrapper').getAnimations()[0]
+const spaceOutAnimation = document.getElementById('wrapper').getAnimations()[0]
+const fadeInAnimation = document.getElementById('wrapper').getAnimations()[1]
 
 // If you click one of the menu anchors
 const anchors = document.querySelectorAll('.nav-item a')
 anchors.forEach(anchor => {
     anchor.addEventListener('click', () => {          
         if (logoDown) {
-            if (!(played) && (anchor.id != 'home')) {
+            if (!(played) && (anchor.id != 'home')) { // Open to article
 
                 logoAnimation.play()
                 menuAnimation.play()
-                divAnimation.play()
+                spaceOutAnimation.play()
+                fadeInAnimation.play()
+
                 logoDown = false
                 played = true
                 lastClick = anchor.id
 
-            } else if ((played) && (anchor.id != lastClick)) {
+            } else if ((played) && (anchor.id != lastClick)) { // Change article
                 
                 logoAnimation.reverse()
                 menuAnimation.reverse()
-                divAnimation.reverse()
+                spaceOutAnimation.reverse()
+                fadeInAnimation.reverse()
+
                 logoDown = false
                 lastClick = anchor.id
 
             }
 
-        } else if ((anchor.id == 'home') && (played)) {
+        } else if ((anchor.id == 'home') && (played)) { // Back to home
 
             logoAnimation.reverse()
             menuAnimation.reverse()
-            divAnimation.reverse()
+            spaceOutAnimation.reverse()
+            fadeInAnimation.reverse()
+
             logoDown = true
             lastClick = anchor.id
 
