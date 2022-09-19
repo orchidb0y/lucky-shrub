562c7f0d49b937bec7bba033d43cb9e9dbbd5d85
David Sobral <davidsobral@me.com>
2022-09-19 11:43:09 -0300

    Added logging and rewrote article animation function

--- js/landing.js
+++ js/landing.js
@@ -8,6 +8,7 @@ const logoAnimation = document.getElementById('top-logo').getAnimations()[0]
 const menuAnimation = document.getElementById('menu').getAnimations()[0]
 const spaceOutAnimation = document.getElementById('wrapper').getAnimations()[0]
 const fadeInAnimation = document.getElementById('wrapper').getAnimations()[1]
+const articleAnimations = document.getElementsByClassName('page-article')
 
 // Animation functions
 function reverseAnimation() {
@@ -17,39 +18,89 @@ function reverseAnimation() {
     fadeInAnimation.reverse()
 }
 
-function playAnimation() {
+function playAnimation(reverse) {
     logoAnimation.play()
     menuAnimation.play()
     spaceOutAnimation.play()
     fadeInAnimation.play()
 }
 
+const delay = ms => new Promise(res => setTimeout(res, ms))
+const animateArticles = async (anchor) => {
+
+    let animation;
+    console.log('Created animation variable')
+
+    if (anchor.id != 'home') {
+        animation = articleAnimations[document.getElementsByClassName(anchor.id + '-article')[0].id].getAnimations()[0]
+        console.log('Animation from', anchor.id + '-article is', animation)
+    }
+
+    if (logoDown) { console.log('Opening from home')
+
+        if (animation.playState == 'paused') { console.log('First time opening articles') // First time opening articles
+            await delay(1000)
+            animation.play()
+        }
+
+    } else if (anchor.id == 'home') { console.log('Closing to home') // Closing article to home
+
+        animation = articleAnimations[document.getElementsByClassName(lastClick + '-article')[0].id].getAnimations()[0]
+        animation.reverse()
+
+    } else if (anchor.id != lastClick) { console.log('Switching article')
+
+        const oldAnimation = articleAnimations[document.getElementsByClassName(lastClick + '-article')[0].id].getAnimations()[0]
+        console.log('Old animation from', lastClick + '-article is', animation)
+        oldAnimation.reverse()
+        await delay(500)
+
+        if (animation.playState == 'paused') { console.log('Article has not been opened before')
+            animation.play()
+        } else if (animation.playState == 'finished') { console.log('Article has been opened before')
+            animation.reverse()
+        }
+
+    }
+
+    lastClick = anchor.id
+    console.log('Changed last click to', lastClick)
+        
+}
+
 // If you click one of the menu anchors
 const anchors = document.querySelectorAll('.nav-item a')
 anchors.forEach(anchor => {
     anchor.addEventListener('click', () => {          
-        if (logoDown) {
-            if (!(played) && (anchor.id != 'home')) { // Open to article
+        console.log('START! Clicked on', anchor.id)
+
+        if (logoDown) {  console.log('Opening from home') // When logo is down, will only listen to clicks on the three buttons to the right
+            if (!(played) && (anchor.id != 'home')) { console.log('Opening article') // Open to article
 
                 playAnimation()
+                animateArticles(anchor)
                 logoDown = false
                 played = true
-                lastClick = anchor.id
 
-            } else if ((played) && (anchor.id != lastClick)) { // Change article (or return from home)
+            } else if ((played) && (anchor.id != lastClick)) { console.log('Returning from home') // Return from home
                 
                 reverseAnimation()
+                animateArticles(anchor)
                 logoDown = false
-                lastClick = anchor.id
 
             }
 
-        } else if ((anchor.id == 'home') && (played)) { // Back to home
+        } else if ((anchor.id == 'home') && (played)) { console.log('Back to home') // Back to home
 
             reverseAnimation()
+            animateArticles(anchor)
             logoDown = true
-            lastClick = anchor.id
+
+        } else if ((anchor.id != lastClick)) { console.log('Switch articles') // Switch articles
+
+            animateArticles(anchor)
 
         }
+
     })
 })
